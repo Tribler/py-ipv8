Tribler/py-ipv8

commit 584cd5fe0b6747fc05778ef51b9e11a9177915d8

Refactored peer lifetime to Churn strategy
Fixes #4

ipv8/peerdiscovery/churn.py
@@ -7,15 +7,41 @@ class RandomChurn(DiscoveryStrategy):
     """
     Select random peers, ping them if inactive, remove them if unresponsive.
     """
-    NODE_REMOVE_TIME = 60.0
 
-    def __init__(self, overlay, sample_size=8, ping_interval=10.0):
+    def __init__(self, overlay, sample_size=8, ping_interval=10.0, inactive_time=27.5, drop_time=57.5):
+        """
+        Random peer removal strategy.
+
+        :param overlay: the overlay to sample peers from
+        :param sample_size: the amount of peers to check at once
+        :param ping_interval: time between pings in the range of inactive_time to drop_time
+        :param inactive_time: time before pings are sent to check liveness
+        :param drop_time: time after which a peer is dropped
+        """
         super(RandomChurn, self).__init__(overlay)
         self._pinged = {}
         self.sample_size = sample_size
         self.ping_interval = ping_interval
+        self.inactive_time = inactive_time
+        self.drop_time = drop_time
 
-    def take_step(self):
+    def should_drop(self, peer):
+        """
+        Have we passed the time before we consider this peer to be unreachable.
+        """
+        if peer.last_response == 0:
+            return False
+        return time() > (peer.last_response + self.drop_time)
+
+    def is_inactive(self, peer):
+        """
+        Have we passed the time before we consider this peer to be inactive.
+        """
+        if peer.last_response == 0:
+            return False
+        return time() > (peer.last_response + self.inactive_time)
+
+    def take_step(self, service_id=None):
         """
         Select a new (set of) peer(s) to investigate liveness for.
         """
@@ -32,9 +58,9 @@ class RandomChurn(DiscoveryStrategy):
             window = sample(self.overlay.network.verified_peers, sample_size)
 
             for peer in window:
-                if peer.should_drop():
+                if self.should_drop(peer) and peer.address in self._pinged:
                     self.overlay.network.remove_peer(peer)
-                elif peer.is_inactive():
+                elif self.is_inactive(peer):
                     if peer.address not in self._pinged:
                         self._pinged[peer.address] = time()
                         packet = self.overlay.create_ping()

ipv8/peer.py
@@ -41,22 +41,6 @@ class Peer(object):
     def get_lamport_timestamp(self):
         return self._lamport_timestamp
 
-    def should_drop(self):
-        """
-        Have we passed the time before we consider this peer to be unreachable.
-        """
-        if self.last_response == 0:
-            return False
-        return time() > (self.last_response + 57.5)
-
-    def is_inactive(self):
-        """
-        Have we passed the time before we consider this peer to be inactive.
-        """
-        if self.last_response == 0:
-            return False
-        return time() > (self.last_response + 27.5)
-
     @property
     def mid(self):
         return self.key.key_to_hash()

commit 2010c43b96016d64feb5436b65795bf7e749643d

Updated unit tests
Also fixed peer drop bug introduced by 584cd5fe0b6747fc05778ef51b9e11a9177915d8

ipv8/peerdiscovery/churn.py
@@ -58,7 +58,7 @@ class RandomChurn(DiscoveryStrategy):
             window = sample(self.overlay.network.verified_peers, sample_size)
 
             for peer in window:
-                if self.should_drop(peer) and peer.address in self._pinged:
+                if self.should_drop(peer):
                     self.overlay.network.remove_peer(peer)
                 elif self.is_inactive(peer):
                     if peer.address not in self._pinged:

test/test_peer.py
@@ -43,31 +43,6 @@ class TestPeer(unittest.TestCase):
 
         self.assertEqual(self.peer.get_lamport_timestamp(), 0)
 
-    def test_intro_inactive(self):
-        """
-        Check if introduced Peers are not be immediately dropped.
-        """
-        self.assertFalse(self.peer.is_inactive())
-        self.assertFalse(self.peer.should_drop())
-
-    def test_inactive(self):
-        """
-        Check if Peers are marked inactive after 30 seconds.
-        """
-        self.peer.last_response = time.time() - 30
-
-        self.assertTrue(self.peer.is_inactive())
-        self.assertFalse(self.peer.should_drop())
-
-    def test_should_drop(self):
-        """
-        Check if Peers are marked should_drop after 60 seconds.
-        """
-        self.peer.last_response = time.time() - 60
-
-        self.assertTrue(self.peer.is_inactive())
-        self.assertTrue(self.peer.should_drop())
-
     def test_peer_equality(self):
         """
         Check if peers with the same key and address are equal.