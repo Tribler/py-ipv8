83302fc8d6e6e72f27848bd360728ae2ca5e1986
Martijn de Vos <mdmartijndevos@gmail.com>
2017-12-06 11:01:10 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IPV8',
+    description='The IPV8 library',
+    version='0.1',
+    url='http://ip-v8.org',
+    package_data={'': ['*.*']},
+    packages=find_packages()
+)
