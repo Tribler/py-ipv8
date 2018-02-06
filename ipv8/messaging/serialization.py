dc09e398d1825fadf8aba6201aa5bb864e23d276
Martijn de Vos <mdmartijndevos@gmail.com>
2018-02-06 13:33:02 +0100

    Added float type to serialization

--- ipv8/messaging/serialization.py
+++ ipv8/messaging/serialization.py
@@ -119,6 +119,7 @@ class Serializer(object):
             'B': DefaultStruct(">B", True),
             'BBH': DefaultStruct(">BBH"),
             'BH': DefaultStruct(">BH"),
+            'f': DefaultStruct(">f", True),
             'H': DefaultStruct(">H", True),
             'HH': DefaultStruct(">HH"),
             'I': DefaultStruct(">I", True),
