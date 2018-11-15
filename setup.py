c64ff42d5fe527c15e24c4abf79ffcff6ef89665
qstokkink <goqs@hotmail.com>
2018-11-15 16:33:15 +0100

    Fixed setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='IPV8',
     description='The IPV8 library',
-    version='0.1',
+    version='1.2',
     url='http://ip-v8.org',
     package_data={'': ['*.*']},
     packages=find_packages()
