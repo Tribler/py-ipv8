b7b83c7607373f4a334af396f8b6df66723e2025
Martijn de Vos <mdmartijndevos@gmail.com>
2019-06-08 11:07:37 +0200

    Crawling now works without knowing chain length
    Our current TrustChain crawling depends on introduction requests that contain an integer with the chain length. However, we cannot always assume that the length of the chain is known when invoking crawl_chain. Therefore, if the latest_block_num argument is None and there is no information about the crawled peer in the database so far, we should first send out a request to fetch the latest block of that peer. By doing so, we find out the chain length and proceed with the crawl.

--- ipv8/test/attestation/trustchain/test_community.py
+++ ipv8/test/attestation/trustchain/test_community.py
@@ -1,17 +1,18 @@
 from __future__ import absolute_import
 
 from six.moves import xrange
+
 from twisted.internet.defer import inlineCallbacks
 
+from ...attestation.trustchain.test_block import TestBlock
+from ...base import TestBase
+from ...mocking.ipv8 import MockIPv8
 from ....attestation.trustchain.block import TrustChainBlock
 from ....attestation.trustchain.caches import CrawlRequestCache
 from ....attestation.trustchain.community import TrustChainCommunity, UNKNOWN_SEQ
 from ....attestation.trustchain.listener import BlockListener
-from ...attestation.trustchain.test_block import TestBlock
 from ....database import database_blob
 from ....keyvault.crypto import default_eccrypto
-from ...base import TestBase
-from ...mocking.ipv8 import MockIPv8
 
 
 class DummyBlock(TrustChainBlock):
@@ -571,6 +572,32 @@ class TestTrustChainCommunity(TestBase):
         yield self.sleep(0.2)  # Let blocks propagate
 
         self.assertEqual(self.nodes[1].overlay.persistence.get_number_of_known_blocks(), 4)
+
+    @inlineCallbacks
+    def test_chain_crawl_unknown_length(self):
+        """
+        Test crawling a chain with unknown length
+        """
+        def create_blocks(num):
+            self.nodes[0].endpoint.close()
+            key = default_eccrypto.generate_key(u'curve25519').pub().key_to_bin()
+            for _ in range(num):
+                self.nodes[0].overlay.sign_block(self.nodes[0].network.verified_peers[0], public_key=key,
+                                                 block_type=b'test', transaction={})
+            self.nodes[0].endpoint.open()
+
+        create_blocks(4)
+
+        yield self.nodes[1].overlay.crawl_chain(self.nodes[0].overlay.my_peer)
+
+        self.assertEqual(self.nodes[1].overlay.persistence.get_number_of_known_blocks(), 4)
+
+        # Now peer 0 create another block, we should be able to get that one too
+        create_blocks(3)
+
+        yield self.nodes[1].overlay.crawl_chain(self.nodes[0].overlay.my_peer)
+
+        self.assertEqual(self.nodes[1].overlay.persistence.get_number_of_known_blocks(), 7)
 
     @inlineCallbacks
     def test_process_block_unrelated_block(self):
