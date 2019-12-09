239912f03b107d8d491b7728312ce3890d23b38d
Martijn de Vos <mdmartijndevos@gmail.com>
2019-12-09 11:41:49 +0100

    Fixed peer inequality check in Python 2.7
    In Python 2.7, there is a slightly different behaviour of the not equal operation (!=) compared to Python 3. In particular, in Python 2.7, statements like peerA == peerB would evaluate to True but statements like peerA != peerB would not evaluate to False, due to the absence of a __ne__ method in the Peer class. This PR fixes this issue and modifies an existing test to test this change.

--- ipv8/test/test_peer.py
+++ ipv8/test/test_peer.py
@@ -50,7 +50,8 @@ class TestPeer(asynctest.TestCase):
         """
         other = Peer(self.peer.key, self.peer.address)
 
-        self.assertEqual(self.peer, other)
+        self.assertTrue(self.peer == other)
+        self.assertFalse(self.peer != other)
 
     def test_peer_inequality_key(self):
         """
