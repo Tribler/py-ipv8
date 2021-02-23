3827b7aefceee614adebaad101b06bec070a8737
Egbert Bouman <ebouman@gmail.com>
2021-02-23 21:49:37 +0100

    Fixed incorrect destination address for puncture-request

--- ipv8/community.py
+++ ipv8/community.py
@@ -254,7 +254,7 @@ class Community(EZPackOverlay):
         if introduced:
             packet = self.create_puncture_request(lan_socket_address, socket_address, identifier, prefix=prefix,
                                                   new_style=new_style)
-            self.endpoint.send(introduction_wan if introduction_lan == ("0.0.0.0", 0) else introduction_lan, packet)
+            self.endpoint.send(introduction.address, packet)
 
         return self._ez_pack(prefix or self._prefix, payload.msg_id, [auth, dist, payload])
 
