c0aaa5036065db86ab48909a20465958593a7843
Quinten Stokkink <goqs@hotmail.com>
2023-10-17 14:00:58 +0200

    Reverted "Added cryptography-based fallback when NaCL import fails
    31b25c7c22437a793a1a65e30f17084553d551bc

--- ipv8/keyvault/private/libnaclkey.py
+++ ipv8/keyvault/private/libnaclkey.py
@@ -1,68 +1,52 @@
+import libnacl
+import libnacl.dual
+import libnacl.sign
+
 from ...keyvault.keys import PrivateKey
 from ...keyvault.public.libnaclkey import LibNaCLPK
 
-try:
-    import libnacl
-    import libnacl.dual
-    import libnacl.sign
 
+class LibNaCLSK(PrivateKey, LibNaCLPK):
+    """
+    A LibNaCL implementation of a secret key.
+    """
 
-    class LibNaCLSK(PrivateKey, LibNaCLPK):
+    def __init__(self, binarykey: bytes = b"") -> None:
         """
-        A LibNaCL implementation of a secret key.
+        Create a new LibNaCL secret key. Optionally load it from a string representation.
+        Otherwise generate it from the 25519 curve.
+
+        :param binarykey: load the sk from this string (see key_to_bin())
         """
+        # Load the key, if specified
+        if binarykey:
+            crypt, seed = (binarykey[:libnacl.crypto_box_SECRETKEYBYTES],
+                           binarykey[libnacl.crypto_box_SECRETKEYBYTES: libnacl.crypto_box_SECRETKEYBYTES
+                                     + libnacl.crypto_sign_SEEDBYTES])
+            key = libnacl.dual.DualSecret(crypt, seed)
+        else:
+            key = libnacl.dual.DualSecret()
 
-        def __init__(self, binarykey: bytes = b"") -> None:
-            """
-            Create a new LibNaCL secret key. Optionally load it from a string representation.
-            Otherwise, generate it from the 25519 curve.
+        super().__init__(pk=key.pk, hex_vk=key.hex_vk())
+        self.key = key
 
-            :param binarykey: load the sk from this string (see key_to_bin())
-            """
-            # Load the key, if specified
-            if binarykey:
-                crypt, seed = (binarykey[:libnacl.crypto_box_SECRETKEYBYTES],
-                               binarykey[libnacl.crypto_box_SECRETKEYBYTES: libnacl.crypto_box_SECRETKEYBYTES
-                                         + libnacl.crypto_sign_SEEDBYTES])
-                key = libnacl.dual.DualSecret(crypt, seed)
-            else:
-                key = libnacl.dual.DualSecret()
-
-            super().__init__(pk=key.pk, hex_vk=key.hex_vk())
-            self.key = key
-
-        def pub(self) -> LibNaCLPK:
-            """
-            Get the public key for this secret key.
-            """
-            return LibNaCLPK(pk=self.key.pk, hex_vk=self.veri.hex_vk())
-
-        def signature(self, msg: bytes) -> bytes:
-            """
-            Create a signature for a message.
-
-            :param msg: the message to sign
-            :return: the signature for the message
-            """
-            return self.key.signature(msg)
-
-        def key_to_bin(self) -> bytes:
-            """
-            Get the string representation of this key.
-            """
-            return b"LibNaCLSK:" + self.key.sk + self.key.seed
-
-except (ImportError, OSError):
-    from .cryptography25519 import Cryptography25519SK
-
-
-    class LibNaCLSK(Cryptography25519SK):  # type: ignore[no-redef]
+    def pub(self) -> LibNaCLPK:
         """
-        Fallback class, actually Cryptography25519SK.
+        Get the public key for this secret key.
         """
+        return LibNaCLPK(pk=self.key.pk, hex_vk=self.veri.hex_vk())
 
-        def pub(self) -> LibNaCLPK:  # type: ignore[override]
-            """
-            Get the public key for this secret key.
-            """
-            return LibNaCLPK(super().key_to_bin()[10:])
+    def signature(self, msg: bytes) -> bytes:
+        """
+        Create a signature for a message.
+
+        :param msg: the message to sign
+        :return: the signature for the message
+        """
+        return self.key.signature(msg)
+
+    def key_to_bin(self) -> bytes:
+        """
+        Get the string representation of this key.
+        """
+        return b"LibNaCLSK:" + self.key.sk + self.key.seed
