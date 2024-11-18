b2298234ef8fd3df5c6039f94303fcd7fd802b1b
qstokkink <goqs@hotmail.com>
2024-11-18 17:22:23 +0100

    Remove Python 3.14 from the testing matrix

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Distributed Computing",
