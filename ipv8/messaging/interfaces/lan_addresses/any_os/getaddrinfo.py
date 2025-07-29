941f845419de95dbc6745b35dd61ce36a5f6e15b
qstokkink <goqs@hotmail.com>
2025-07-29 10:35:10 +0200

    Fixes uncaught getaddrinfo UnicodeError

--- ipv8/messaging/interfaces/lan_addresses/any_os/getaddrinfo.py
+++ ipv8/messaging/interfaces/lan_addresses/any_os/getaddrinfo.py
@@ -12,22 +12,26 @@ class SocketGetAddrInfo(AddressProvider):
     def get_addresses(self) -> set:
         """
         Attempt to use ``getaddrinfo()`` to retrieve addresses.
+
+        Ref. ``UnicodeError``: https://github.com/python/cpython/issues/77139.
+
+        :returns: The set of probable local interfaces.
         """
         interface_specifications = []
 
         try:
             interface_specifications.extend(socket.getaddrinfo(socket.getfqdn(), 0))
-        except OSError:
+        except (OSError, UnicodeError):
             self.on_exception()
 
         try:
             interface_specifications.extend(socket.getaddrinfo(socket.gethostname(), None))
-        except OSError:
+        except (OSError, UnicodeError):
             self.on_exception()
 
         try:
             interface_specifications.extend(socket.getaddrinfo(None, 0))
-        except OSError:
+        except (OSError, UnicodeError):
             self.on_exception()
 
-        return {i[4][0] for i in interface_specifications if cast(str, i[4][0]).find(".") != -1}
+        return {i[4][0] for i in interface_specifications if cast("str", i[4][0]).find(".") != -1}
